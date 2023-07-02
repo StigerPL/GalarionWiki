46cd17d3761efb9aca87edb8fb4a9633cbc67403
StigerPL <85870161+StigerPL@users.noreply.github.com>
2023-07-02 21:37:26 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,7 @@ function checking(form)
   username = new Array("ja");
   password = new Array("galarion");
 
-  //for(int i = 0; i<username.length;i++)
+  //for(int i = 0; i<username.length;i++)a
   //{
       if(form.userid.value == username[0]  && form.pswrd.value == password[0])
       {
