1ee810d4cb15321e547aac266273c2fd5d154a40
StigerPL <85870161+StigerPL@users.noreply.github.com>
2023-07-02 21:34:43 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -5,7 +5,7 @@ function checking(form)
 
   //for(int i = 0; i<username.length;i++)
   //{
-      if(form.userid.value == username[i]  && form.pswrd.value == password[i])
+      if(form.userid.value == username[0]  && form.pswrd.value == password[0])
       {
           location.replace('home.html')
       }
