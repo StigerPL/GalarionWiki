3a6e2606668497e2aa1fbef57d123c988fe2d75f
StigerPL <85870161+StigerPL@users.noreply.github.com>
2023-07-02 20:25:54 +0200

    Create login.js

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+function check(form) {
+  username = new Array("ja");
+  password = new Array("galarion");
+
+  for(int i = 0; i<username.length;i++)
+  {
+      if(form.userid.value = username[i]  && form.pswrd.value == password[i])
+      {
+          location.replace("home.html");
+      }
+      else
+      {
+          alert("Incorrect Username or Password");
+      }
+  }
+}
